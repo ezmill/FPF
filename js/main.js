8108066645fe35ac6d51288a49ed1c8be46397b8
Ezra Miller <emiller@lsoc.org>
2014-05-20 13:17:00 -0500

    testing 1-2

--- js/main.js
+++ js/main.js
@@ -1,12 +1,30 @@
 (function(){
 	var app = angular.module("fpf",[]);
 	app.controller("BlogController",function(){
-		this.posts = entries;
+		this.entries = entries;
 	});
+	app.controller("WorksController",function(){
+		this.books = works;
+	});
+	app.controller("PostController",function(){
+		this.post = {};
+		this.addPost = function(){
+			this.post.date = Date.now();
+			entries.push(this.post);
+			this.post = {};
+		}
+	});
+
 	function Post(title,date){
 		this.title = title;
 		this.date = date;
 		this.content = "";
+	};
+	function Work(title,date,image){
+		this.title = title;
+		this.date = date;
+		this.image = image
+		this.description = "";
 	};
 	Post.prototype.hasContent = function() {
 		if(this.content){
@@ -15,9 +33,18 @@
 			return false;
 		}
 	};
+	Work.prototype.addDescription = function(){
+
+	}
 	var entry1 = new Post("Sample Post", "May 19th, 6:29 PM");
 	entry1.content = "This is a sample post to help me learn angular."
 	var entry2 = new Post("Sample Post", Date.now());
 	entry2.content = "Just another sample post."
 	var entries = [entry1,entry2];
+	var work1 = new Work("Book 1", Date.now(), "img/1.jpg");
+	work1.description = "Pretty good, by me."
+	var work2 = new Work("Book 2", Date.now(), "img/2.jpg");
+	work2.description = "Pretty good, by me."
+
+	var works =  [work1,work2]
 }());
